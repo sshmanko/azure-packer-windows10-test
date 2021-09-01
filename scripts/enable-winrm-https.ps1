9f37daf5a085d61491cd81fce2a2a405e6459287
Sergei Shmanko <sergei.shmanko@rangeforce.com>
2021-09-01 13:20:25 +0300

    add winrm-https

---
+++ scripts/enable-winrm-https.ps1
@@ -0,0 +1,6 @@
+Enable-PSRemoting -SkipNetworkProfileCheck -Force
+New-NetFirewallRule -Name "Allow WinRM HTTPS" -DisplayName "WinRM HTTPS" -Enabled True -Profile Any -Action Allow -Direction Inbound -LocalPort 5986 -Protocol TCP
+$thumbprint = (New-SelfSignedCertificate -DnsName $env:COMPUTERNAME -CertStoreLocation Cert:\LocalMachine\My).Thumbprint
+$command = "winrm create winrm/config/Listener?Address=*+Transport=HTTPS @{Hostname=""$env:computername""; CertificateThumbprint=""$thumbprint""}"
+cmd.exe /C $command
+Enable-WSManCredSSP -Role Server -Force
