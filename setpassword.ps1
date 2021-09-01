24c79ad7a5a7b8f785216a0880cbc09b684264b8
Sergei Shmanko <sergei.shmanko@rangeforce.com>
2021-09-01 12:06:30 +0300

    typo

--- setpassword.ps1
+++ setpassword.ps1
@@ -1,2 +1,2 @@
-(Get-Content d:\packer\windows10.json) -replace 'Password123', $args[0] | Set-Content d:\packer\windows10.json
+(Get-Content d:\packer\windows_10.json) -replace 'Password123', $args[0] | Set-Content d:\packer\windows10.json
 (Get-Content d:\packer\answer_files\Autounattend.xml) -replace 'Password123', $args[0] | Set-Content d:\packer\answer_files\Autounattend.xml
