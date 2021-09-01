59cb8adf23a3be9c9ee0e18940fc66d9165c0286
Sergei Shmanko <sergei.shmanko@rangeforce.com>
2021-09-01 14:12:25 +0300

    use https winrm

--- scripts/enable-winrm.ps1
+++ scripts/enable-winrm.ps1
@@ -1,17 +1,11 @@
-$NetworkListManager = [Activator]::CreateInstance([Type]::GetTypeFromCLSID([Guid]"{DCB00C01-570F-4A9B-8D69-199FDBA5723B}"))
-$Connections = $NetworkListManager.GetNetworkConnections()
-$Connections | ForEach-Object { $_.GetNetwork().SetCategory(1) }
+Enable-PSRemoting -SkipNetworkProfileCheck -Force
 
-Enable-PSRemoting -Force
-winrm quickconfig -q
-winrm quickconfig -transport:http
-winrm set winrm/config '@{MaxTimeoutms="1800000"}'
-winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="800"}'
-winrm set winrm/config/service '@{AllowUnencrypted="true"}'
-winrm set winrm/config/service/auth '@{Basic="true"}'
-winrm set winrm/config/client/auth '@{Basic="true"}'
-winrm set winrm/config/listener?Address=*+Transport=HTTP '@{Port="5985"}'
-netsh advfirewall firewall set rule group="Windows Remote Administration" new enable=yes
-netsh advfirewall firewall set rule name="Windows Remote Management (HTTP-In)" new enable=yes action=allow
+$thumbprint = (New-SelfSignedCertificate -DnsName $env:COMPUTERNAME -CertStoreLocation Cert:\LocalMachine\My).Thumbprint
+winrm set winrm/config/listener?Address=*+Transport=HTTPS @{Hostname="$env:computername"; CertificateThumbprint="$thumbprint"}'
+
+netsh advfirewall firewall add rule name="Windows Remote Management (HTTPS-In)" dir=in action=allow protocol=TCP localport=5986
+
+Enable-WSManCredSSP -Role Server -Force
+
 Set-Service winrm -startuptype "auto"
 Restart-Service winrm
