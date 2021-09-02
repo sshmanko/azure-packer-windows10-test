8aecb1f21e002edd9869656c4e25fae945a18927
Sergei Shmanko <sergei.shmanko@rangeforce.com>
2021-09-02 10:44:33 +0300

    add optimize_powershell.ps1

---
+++ scripts/optimize_powershell.ps1
@@ -0,0 +1,18 @@
+# https://docs.ansible.com/ansible/latest/user_guide/windows_performance.html
+
+function Optimize-PowershellAssemblies {
+  # NGEN powershell assembly, improves startup time of powershell by 10x
+  $old_path = $env:path
+  try {
+    $env:path = [Runtime.InteropServices.RuntimeEnvironment]::GetRuntimeDirectory()
+    [AppDomain]::CurrentDomain.GetAssemblies() | % {
+      if (! $_.location) {continue}
+      $Name = Split-Path $_.location -leaf
+      Write-Progress -Activity "Native Image Installation" -Status "$name"
+      ngen install $_.location | % {"`t$_"}
+    }
+  } finally {
+    $env:path = $old_path
+  }
+}
+Optimize-PowershellAssemblies
