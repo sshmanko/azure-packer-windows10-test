04b641764238fd628fad53bc7d4987d8ad166ec8
Sergei Shmanko <sergei.shmanko@rangeforce.com>
2021-09-02 20:14:38 +0300

    add optimize cmd

--- build.ps1
+++ build.ps1
@@ -11,6 +11,7 @@ $endDTM = (Get-Date)
 Write-Host "[INFO]  - Elapsed Time: $(($endDTM-$startDTM).totalseconds) seconds" -ForegroundColor Yellow
 
 Convert-VHD -Path "D:\packer\output\Virtual Hard Disks\packer-vm.vhdx" -DestinationPath "D:\packer\output\Virtual Hard Disks\packer-vm.vhd" -VHDType Fixed
+Optimize-VHD -Path "D:\packer\output\Virtual Hard Disks\packer-vm.vhd" -Mode Full
 (Get-VHD 'D:\packer\output\Virtual Hard Disks\packer-vm.vhd').FileSize | Out-File -FilePath D:\disksize
 
 
