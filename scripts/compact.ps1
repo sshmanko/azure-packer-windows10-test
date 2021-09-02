f82770add0f149218dd17680bf6b9b86b621d76c
Sergei Shmanko <sergei.shmanko@rangeforce.com>
2021-09-02 17:09:43 +0300

    add sleep

--- scripts/compact.ps1
+++ scripts/compact.ps1
@@ -3,3 +3,6 @@ Get-ChildItem "C:\Windows\SoftwareDistribution\*" -Recurse -Force -ErrorAction S
 Get-ChildItem "C:\Windows\Temp\*" -Recurse -Force  -ErrorAction SilentlyContinue | Remove-Item -Force -recurse -ErrorAction SilentlyContinue
 Get-ChildItem "C:\users\*\AppData\Local\Temp\*" -Recurse -Force -ErrorAction SilentlyContinue | Remove-Item -Force -recurse -ErrorAction SilentlyContinue
 Get-ChildItem "C:\users\*\AppData\Local\Microsoft\Windows\Temporary Internet Files\*" -Recurse -Force -ErrorAction SilentlyContinue | Remove-Item -Force -recurse -ErrorAction SilentlyContinue
+
+# Finish background tasks
+Start-Sleep -s 900
